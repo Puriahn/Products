266a9fa1bbee51199d04f2464d58bdfeb458374d
Puriahn <119783351+Puriahn@users.noreply.github.com>
2024-07-31 14:10:59 +0330

    shopCart

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,5 +1,6 @@
 import useHttp from "../Hooks/useHttp";
 import Error from "../Common/Error";
+import Button from "../Common/Button";
 
 const w = {};
 
@@ -17,13 +18,19 @@ export default function ProductList() {
     return <Error title="Failed to fetch data" message={error} />;
   }
   return (
-   
-      <ul className="bg-slate-500 mx-48 rounded-lg flex-row text-center my-4">
-        {data.map((album) => (
-          <li className="my-5 items-center px-24" key={album.id}>
-            <article>{album.title}</article>
-          </li>
-        ))}
-      </ul>
+    <ul className="bg-slate-500 mx-24 rounded-lg flex-row text-center my-4">
+      {data.map((album) => (
+        <li className="my-5 items-center pb-9" key={album.id}>
+          <article>
+            <span className="font-bold">ID: </span> {album.id}{" "}
+            <span className="font-bold">___ TITLE:  </span>
+            {album.title}{" "}
+            <span>
+              <Button>Afdd</Button>
+            </span>
+          </article>
+        </li>
+      ))}
+    </ul>
   );
 }
